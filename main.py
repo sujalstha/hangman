b3d68e9d5e6110fee5f9198d6e92d406b179791a
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-18 16:04:58 -0500

    Commit initial

--- main.py
+++ main.py
@@ -13,11 +13,29 @@ word = random.choices(words)
 for i in word:
     word = i
 
+turn = 6
+
 while True:
-    failed = 0
+    incorrect = 0
 
     for char in word:
         if char in word:
             print(char)
         else:
             print("-")
+            incorrect += 1
+
+    if turn == 0:
+        print("You Win")
+        print("The word is: ", word)
+        break
+
+    guess = input("guess a letter: ")
+    guess += guess
+
+    if guess not in word:
+        turn -= 1
+        print("Wrong Try Again\nYou have", incorrect)
+
+
+
