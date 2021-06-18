587a3c597413728e8a80f4d213877caec169329d
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-18 14:22:31 -0500

    Commit initial

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ import random
 import time
 
 words = ['Opera', 'Zeppelin', 'Jazz', 'Juneteenth', 'Github']
-
+chances = 6
 
 name = input("What is your name? ")
 time.sleep(1)
-print("Hello, " + name, "Time to play hangman!")
+print("Hello, " + name, "its time to play hangman!")
+time.sleep(1)
+print('start guessing')
+
+word = random.choices(words)
+
+for i in word:
+    word = i
+
+print(word)
