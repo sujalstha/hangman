91b138a15a7422f34a4ef13570054495af996e9e
Sujal Shrestha <sujalshrestha2004@gmail.com>
2021-06-19 16:34:34 -0500

    Commit initial

--- main.py
+++ main.py
@@ -15,29 +15,23 @@ guesses = ''
 for word in ele_word:
     ele_word = word
 
-while True:
-    incorrect = 6
+turn = 15
 
+while turn > 0:
     for letter in ele_word:
         if letter in guesses:
             print(letter)
         else:
-            print("-")
+            print('-')
 
     guess = input("guess a letter: ")
     guesses += guess
 
     if guess not in ele_word:
-        incorrect -= 1
-        print("\nWrong Try Again\nYou have", incorrect, "guesses.")
+        turn -= 1
+        print("\nWrong Try Again\nYou have", turn, "guesses.")
 
     if turn == 0:
         print("Game Over, sorry you lost :C")
-        break
-
-    if guesses == ele_word:
-        print("\nYou Win")
-        print("\nThe word is: ", ele_word)
-        break
 
 
